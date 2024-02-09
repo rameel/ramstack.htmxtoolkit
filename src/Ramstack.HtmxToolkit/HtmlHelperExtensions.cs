98f6cfb47b055afc3205d77960471c642eac3f57
rameel <rameel-b@hotmail.com>
2024-02-10 03:40:38 +0600

    Remove wrapping <script> tag

--- src/Ramstack.HtmxToolkit/HtmlHelperExtensions.cs
+++ src/Ramstack.HtmxToolkit/HtmlHelperExtensions.cs
@@ -10,8 +10,8 @@ namespace Ramstack.HtmxToolkit;
 /// </summary>
 public static class HtmlHelperExtensions
 {
-    private static readonly HtmlString _script = new($"<script>{HtmxAssets.Script}</script>");
-    private static readonly HtmlString _debugScript = new($"<script>{HtmxAssets.DebugScript}</script>");
+    private static readonly HtmlString _script = new(HtmxAssets.Script);
+    private static readonly HtmlString _debugScript = new(HtmxAssets.DebugScript);
 
     /// <summary>
     /// Gets the HTML string that represents the path to the minified version of the javascript.
@@ -24,20 +24,18 @@ public static class HtmlHelperExtensions
     internal static HtmlString DebugPath { get; set; } = new(EndpointRouteBuilderExtensions.AssetPath + "?debug");
 
     /// <summary>
-    /// Returns an HTML string of the script tag containing the javascript for htmx
-    /// to integrate with the anti-forgery feature of ASP.NET Core.
+    /// Returns an HTML string containing the javascript for htmx to integrate with the anti-forgery feature of ASP.NET Core.
     /// </summary>
     /// <param name="_">The <see cref="IHtmlHelper"/> instance that this method extends.</param>
     /// <param name="debug">A boolean value indicating whether to use the debug version of the javascript.</param>
     /// <returns>
-    /// An HTML string of the script tag.
+    /// An HTML string of the script.
     /// </returns>
     public static IHtmlContent HtmxAntiforgeryScript(this IHtmlHelper _, bool debug = false) =>
         debug ? _debugScript : _script;
 
     /// <summary>
-    /// Returns an HTML string of the path to the javascript
-    /// to integrate with the anti-forgery feature of ASP.NET Core.
+    /// Returns an HTML string of the path to the javascript to integrate with the anti-forgery feature of ASP.NET Core.
     /// </summary>
     /// <param name="_">The <see cref="IHtmlHelper"/> instance that this method extends.</param>
     /// <param name="debug">A boolean value indicating whether to use the debug version of the javascript.</param>
