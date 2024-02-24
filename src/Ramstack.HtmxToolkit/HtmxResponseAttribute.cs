4060ac12cec23a677ec3621c7a4ab828a41a8d2d
rameel <rameel-b@hotmail.com>
2024-02-25 00:29:41 +0600

    Remove ReturnPartial functionality

--- src/Ramstack.HtmxToolkit/HtmxResponseAttribute.cs
+++ src/Ramstack.HtmxToolkit/HtmxResponseAttribute.cs
@@ -74,11 +74,6 @@ public sealed class HtmxResponseAttribute : Attribute, IResultFilter
     /// </summary>
     public bool StopPolling { get; set; }
 
-    /// <summary>
-    /// Gets or sets a value that determines whether to return a partial view for htmx requests.
-    /// </summary>
-    public bool ReturnPartial { get; set; }
-
     /// <inheritdoc />
     public void OnResultExecuting(ResultExecutingContext context)
     {
@@ -91,9 +86,6 @@ public sealed class HtmxResponseAttribute : Attribute, IResultFilter
             var headers = response.Headers;
             foreach (ref var kvp in CollectionsMarshal.AsSpan(_headers))
                 headers[kvp.Key] = kvp.Value;
-
-            if (ReturnPartial)
-                context.Result = context.Result.ToPartialViewResult();
         }
     }
 
--- src/Ramstack.HtmxToolkit/HtmxResult.cs
+++ src/Ramstack.HtmxToolkit/HtmxResult.cs
@@ -8,35 +8,15 @@ namespace Ramstack.HtmxToolkit;
 /// </summary>
 public sealed class HtmxResult(IActionResult result, Action<HtmxResponse> configure) : IActionResult
 {
-    /// <summary>
-    /// Gets or sets a value that determines whether to return a partial view for htmx requests.
-    /// </summary>
-    public bool ReturnPartial { get; set; }
-
     /// <inheritdoc />
     public Task ExecuteResultAsync(ActionContext context)
     {
         if (context.HttpContext.Request.IsHtmxRequest())
         {
-            if (ReturnPartial)
-                result = result.ToPartialViewResult();
-
             var response = new HtmxResponse(context.HttpContext.Response);
             configure(response);
         }
 
         return result.ExecuteResultAsync(context);
-    }
-
-    /// <summary>
-    /// Converts the <see cref="ViewResult"/> to the <see cref="PartialViewResult"/> for htmx requests.
-    /// </summary>
-    /// <returns>
-    /// The <see cref="HtmxResult{TState}"/>.
-    /// </returns>
-    public HtmxResult ToPartialView()
-    {
-        ReturnPartial = true;
-        return this;
     }
 }
