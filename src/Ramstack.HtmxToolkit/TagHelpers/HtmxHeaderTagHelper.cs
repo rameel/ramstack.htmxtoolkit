94676e18836f97889db6829bae35ce54076c2820
rameel <rameel-b@hotmail.com>
2026-08-09 14:41:47 +0500

    Use file-scoped namespace

--- src/Ramstack.HtmxToolkit/TagHelpers/HtmxHeaderTagHelper.cs
+++ src/Ramstack.HtmxToolkit/TagHelpers/HtmxHeaderTagHelper.cs
@@ -5,40 +5,39 @@ using Microsoft.AspNetCore.Razor.TagHelpers;
 
 using Ramstack.HtmxToolkit.Internal;
 
-namespace Ramstack.HtmxToolkit.TagHelpers
+namespace Ramstack.HtmxToolkit.TagHelpers;
+
+/// <summary>
+/// Represents a <see cref="TagHelper"/> implementation that applies the <c>hx-headers</c> attribute to matching elements.
+/// </summary>
+/// <remarks>
+/// <list type="bullet">
+///   <item><c>hx-headers</c> is inherited and can be placed on a parent element.</item>
+///   <item>A child declaration of a header overrides a parent declaration.</item>
+/// </list>
+/// </remarks>
+[HtmlTargetElement(Attributes = HeadersDictionaryName)]
+[HtmlTargetElement(Attributes = HeadersPrefix + "*")]
+public sealed class HtmxHeaderTagHelper : TagHelper
 {
+    private const string HeadersPrefix = "hx-header-";
+    private const string HeadersDictionaryName = "hx-all-headers";
+
     /// <summary>
-    /// Represents a <see cref="TagHelper"/> implementation that applies the <c>hx-headers</c> attribute to matching elements.
+    /// Gets or sets the <c>hx-header</c> attribute values.
     /// </summary>
-    /// <remarks>
-    /// <list type="bullet">
-    ///   <item><c>hx-headers</c> is inherited and can be placed on a parent element.</item>
-    ///   <item>A child declaration of a header overrides a parent declaration.</item>
-    /// </list>
-    /// </remarks>
-    [HtmlTargetElement(Attributes = HeadersDictionaryName)]
-    [HtmlTargetElement(Attributes = HeadersPrefix + "*")]
-    public sealed class HtmxHeaderTagHelper : TagHelper
+    [HtmlAttributeName(HeadersDictionaryName, DictionaryAttributePrefix = HeadersPrefix)]
+    public Dictionary<string, string> Headers { get; set; } = new();
+
+    /// <inheritdoc />
+    public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
-        private const string HeadersPrefix = "hx-header-";
-        private const string HeadersDictionaryName = "hx-all-headers";
+        var headers = new HtmlString(
+            JsonSerializer.Serialize(Headers, JsonOptions.PreserveKeyCase));
 
-        /// <summary>
-        /// Gets or sets the <c>hx-header</c> attribute values.
-        /// </summary>
-        [HtmlAttributeName(HeadersDictionaryName, DictionaryAttributePrefix = HeadersPrefix)]
-        public Dictionary<string, string> Headers { get; set; } = new();
+        output.Attributes.SetAttribute(
+            new TagHelperAttribute("hx-headers", headers, HtmlAttributeValueStyle.SingleQuotes));
 
-        /// <inheritdoc />
-        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
-        {
-            var headers = new HtmlString(
-                JsonSerializer.Serialize(Headers, JsonOptions.PreserveKeyCase));
-
-            output.Attributes.SetAttribute(
-                new TagHelperAttribute("hx-headers", headers, HtmlAttributeValueStyle.SingleQuotes));
-
-            return Task.CompletedTask;
-        }
+        return Task.CompletedTask;
     }
 }
